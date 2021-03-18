047eb8029c3eae2b2e8638e371c9eaf7bff5e259
yuxt <yuxt@live.com>
2021-03-18 23:30:01 +0800

    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ with st.echo(code_location='below'):
 
     for curr_point_num in range(total_points):
         curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pa * i / points_per_turn
+        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
         radius = curr_point_num / total_points
         x = radius * math.cos(angle)
         y = radius * math.sin(angle)
